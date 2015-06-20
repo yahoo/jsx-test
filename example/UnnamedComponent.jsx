f2ec88d54224ca3588fb6bbc65351b76554b17e9
stanleyn <stanleyhlng@yahoo.com.hk>
2015-06-20 00:54:34 -0700

    require JSX files that can be covered by istanbul.

---
+++ example/UnnamedComponent.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var UnnamedComponent = React.createClass({
+    render: function() {
+        return <div>UNNAMED_COMPONENT</div>;
+    }
+});
+
+module.exports = UnnamedComponent;
