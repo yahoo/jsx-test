6d3eb77c76785bbdeb7faaeb22ee3d559c347a3b
Hank Hsiao <hankxiao@yahoo-inc.com>
2015-06-22 10:22:53 -0700

    add tests for unmountComponent

--- test/renderComponent.test.js
+++ test/renderComponent.test.js
@@ -19,9 +19,12 @@ var Component = React.createClass({
 describe('#renderComponent', function() {
     it('renders props & children on the component', function () {
         var comp = jsx.renderComponent(Component, { foo: 'bar' }, 'child');
-        
+
         // Verify that it's Off by default
         assert.equal(comp.getDOMNode().textContent, 'child');
         assert.equal(comp.getDOMNode().getAttribute('data-foo'), 'bar');
+
+        jsx.unmountComponent(comp);
+        assert.notEqual(comp.isMounted(), true);
     });
 });
