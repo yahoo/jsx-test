b295e0419836cc621452684f8ab040817b6a00bc
Jeff Kolesky <jeff@kolesky.com>
2015-07-13 21:25:42 -0700

    Adds tests for querySelector and simulateEvent

---
+++ test/elementQuerySelector.test.js
@@ -0,0 +1,37 @@
+// jsx-test
+var jsx = require('../index');
+var React = require('react');
+var assert = require('assert');
+
+var Component = React.createClass({
+    displayName: 'TestComp',
+    render: function () {
+        var props = {
+            className: 'test-class'
+        };
+        var children = [
+            React.createElement('li', props),
+            React.createElement('li', props),
+            React.createElement('li', props),
+        ];
+        return React.createElement('ul', {}, children);
+    }
+});
+
+describe('#elementQuerySelector', function () {
+    it('finds the element on the component', function () {
+        var comp = jsx.renderComponent(Component);
+
+        var child = jsx.elementQuerySelector(comp, '.test-class');
+        assert.equal(child.tagName, 'LI');
+    });
+});
+
+describe('#elementQuerySelectorAll', function () {
+    it('finds the element on the component', function () {
+        var comp = jsx.renderComponent(Component);
+
+        var children = jsx.elementQuerySelectorAll(comp, '.test-class');
+        assert.equal(children.length, 3);
+    });
+});
